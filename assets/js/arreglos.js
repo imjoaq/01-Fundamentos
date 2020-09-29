0cd46c759b8a5d68f6f2fd62d13030f847f16a05
Alfonso Ramírez <imjoaq@users.noreply.github.com>
2020-09-29 00:44:21 -0400

    things

--- assets/js/arreglos.js
+++ assets/js/arreglos.js
@@ -1,8 +1,23 @@
 // const arr = new Array(10);
-const arr = [];
-console.log(arr);
+// const arr = [];
+// console.log(arr);
 
-let alf = 'juanito ',
-  fla = 'maria';
+// let videoJuegos = ['Mario 3', 'Megaman', 'Chrono Trigger'];
+// console.log([videoJuegos]);
 
-console.log(alf + fla);
+// console.log(videoJuegos[0]);
+
+let arregloCosas = [
+  true,
+  123,
+  'Fernando',
+  1 + 2,
+  function () {},
+  () => {},
+  { a: 1 },
+  ['X', 'Megaman', 'Zero', 'Dr.Light', ['Dr.Willy', 'Woodman']],
+];
+
+// console.log({ arregloCosas });
+
+console.log(arregloCosas[7][4][1]);
