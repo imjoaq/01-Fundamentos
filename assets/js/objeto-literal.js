77cfd8dd926d93cc2005a67756a54fcfef0f610f
Alfonso Ramírez <imjoaq@users.noreply.github.com>
2020-10-05 17:24:25 -0400

    .

--- assets/js/objeto-literal.js
+++ assets/js/objeto-literal.js
@@ -1,4 +1,4 @@
-let personaje = {
+const personaje = {
   nombre: 'Tony Stark',
   codeName: 'ironman',
   vivo: false,
@@ -12,7 +12,7 @@ let personaje = {
     zip: '10880, 90265',
     ubicacion: 'Malibu, California',
   },
-  'ultima-pelicula': 'Infinity War'
+  'ultima-pelicula': 'Infinity War',
 };
 
 console.log(personaje);
@@ -24,12 +24,23 @@ console.log('coords', personaje.coords);
 console.log('Lat', personaje.coords.lat);
 
 console.log('No. Trajes', personaje.trajes.length);
-console.log(
-  'Ultimo traje Trajes',
-  personaje.trajes[personaje.trajes.length - 1]
-);
+console.log('Ultimo traje', personaje.trajes[personaje.trajes.length - 1]);
 
 const x = 'vivo';
 console.log('Vivo:', personaje[x]);
 
-console.log('Ultima Pelicula:', personaje["ultima-pelicula"]);
+console.log('Ultima Pelicula:', personaje['ultima-pelicula']);
+
+//Detalles
+
+delete personaje.edad;
+console.log(personaje);
+
+personaje.casado = true;
+
+const entriesPares = Object.entries(personaje);
+console.log(entriesPares);
+
+console.log(personaje);
+
+Object.freeze(personaje);
