8f887ff57608cfebdeff3c9dcf5b14a14246d048
Alfonso Ramírez <imjoaq@users.noreply.github.com>
2020-10-16 12:05:40 -0400

    changes

--- assets/js/logica-booleana.js
+++ assets/js/logica-booleana.js
@@ -2,3 +2,13 @@ const regresaTrue = () => {
   console.log('Regresa True');
   return true;
 };
+
+const regresaFalse = () => {
+  console.log('Regresa False');
+  return false;
+};
+
+console.warn('Not o la negacion');
+console.log(true); // true
+console.log(!true); // false
+console.log(!false); // true
