c800a3758bdd8aa30086d6d66d6473491d1678ff
Alfonso Ramírez <imjoaq@users.noreply.github.com>
2020-10-16 19:48:29 -0400

    update

--- assets/js/operador-ternario.js
+++ assets/js/operador-ternario.js
@@ -1,6 +1,6 @@
-//*
-* Dias de semana abrimos a las 11,
-* pero los fines de semana abrimos a las 9
-*/
+// //*
+//  Dias de semana abrimos a las 11,
+//  pero los fines de semana abrimos a las 9
+//
 
-// Entra a un sitio web, para consultar si esta abierto hoy.... 
+// Entra a un sitio web, para consultar si esta abierto hoy....
